5f6ce2bc9b5e2d23ca44862f47a6da237f83c795
thefalkon-1 <82978130+thefalkon-1@users.noreply.github.com>
2024-01-25 09:44:12 -0500

    Update app.R

--- air-yards-leaderboard/app.R
+++ air-yards-leaderboard/app.R
@@ -6,7 +6,7 @@ library(reactablefmtr)
 library(MetBrewer)
 
 # Read in dataset
-new_df <- readRDS(gzcon(url("https://github.com/thefalkon-1/Prayer-Yards-Project/raw/main/data/data010424.rds")))
+new_df <- readRDS(gzcon(url("https://github.com/thefalkon-1/Prayer-Yards-Project/raw/main/data/2023_regular_season.rds")))
 
 max_week <- max(new_df$week)
 
